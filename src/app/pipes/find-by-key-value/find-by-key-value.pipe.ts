da3dfe0166d4b4fed4424f3f911a3e8078837bdf
Holy Ghost <subashsapkota59@gmail.com>
2021-07-31 13:21:56 +0545

    fix: element type not loading on initial table cell click

--- src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
+++ src/app/pipes/find-by-key-value/find-by-key-value.pipe.ts
@@ -4,9 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'findByKeyValue'
 })
 export class FindByKeyValuePipe implements PipeTransform {
-
-  transform(value: Array<{[key: string]: any}>, key: string, cmpValue: string): any {
-    return value.find(val => val[key] === cmpValue);
+  transform(value: Array<{ [key: string]: any }>, key: string, cmpValue: string): any {
+    return value.find((val) => val[key] === cmpValue);
   }
-
 }
